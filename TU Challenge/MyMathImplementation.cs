74c2dd4068a12f5e37d64c69c8c16ded26bca4b5
EmilieGonin <emilie.yuu@gmail.com>
2023-01-27 21:28:20 +0100

    create all blank functions

--- TU Challenge/MyMathImplementation.cs
+++ TU Challenge/MyMathImplementation.cs
@@ -6,8 +6,76 @@ using System.Threading.Tasks;
 
 namespace TU_Challenge
 {
-    public class MyMathImplementation
+    public static class MyMathImplementation
     {
-        
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static bool IsMajeur(int age)
+        {
+            //
+        }
+
+        public static bool isEven(int a)
+        {
+            //
+        }
+
+        public static bool IsDivisible(int a, int b)
+        {
+            //
+        }
+
+        public static bool IsPrimary(int a)
+        {
+            //
+        }
+
+        public static List<int> GetAllPrimary(int a)
+        {
+            //
+        }
+
+        public static int Power2(int a)
+        {
+            //
+        }
+
+        public static int Power(int a, int b)
+        {
+            //
+        }
+
+        public static int IsInOrder(int a, int b)
+        {
+            //
+        }
+
+        public static int IsInOrderDesc(int a, int b)
+        {
+            //
+        }
+
+        public static bool IsListInOrder(List<int> list)
+        {
+            //
+        }
+
+        public static List<int> Sort(List<int> list)
+        {
+            //
+        }
+
+        public static List<int> GenericSort(List<int> list, MyMathImplementation.IsInOrder)
+        {
+            //
+        }
+
+        public static List<int> GenericSort(List<int> list, MyMathImplementation.IsInOrderDesc)
+        {
+            //
+        }
     }
 }
